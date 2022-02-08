4942e01894ec2fc2c952e3e69ed80e25954d9384
spman2330 <vuhoainam4040@gmail.com>
2022-02-08 23:35:23 +0700

    update_readme

--- Elemont/Program.cs
+++ Elemont/Program.cs
@@ -19,7 +19,7 @@ namespace Elemont
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FSpeciesEdit());
+            Application.Run(new fSkillEdit());
         }
     }
 }
